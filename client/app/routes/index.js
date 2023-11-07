fb19ec2451f6362db2f32c3636cec4dceb314242
Shane Jones <shane@Shanes-MacBook-Pro.local>
2023-11-07 18:05:05 -0500

    adding api host log to index route

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,11 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import ENV from 'client/config/environment';
 
 export default class IndexRoute extends Route {
     @service session;
     @service router
 
     beforeModel() {
+      console.log('ENV.API_HOST value:', ENV.API_HOST);
         if (this.session.isAuthenticated) {
           this.router.transitionTo('games');
         }

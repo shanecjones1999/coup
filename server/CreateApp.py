23423598e0294cbcfac2c0208f91f32dbbfd88e1
EC2 Default User <ec2-user@ip-172-31-17-8.us-east-2.compute.internal>
2023-12-14 00:58:38 +0000

    Add default app route

--- server/CreateApp.py
+++ server/CreateApp.py
@@ -4,3 +4,9 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "topSecret"
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    print("Reached this point")
+    return app.send_static_file("index.html")
